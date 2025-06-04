83efead5987c7099157c8d114af92a7b5a3ffcba
Guido Salazar Vargas <guido.salazar.vargas7@gmail.com>
2025-06-04 01:15:51 -0400

    Options fix in the params

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -113,7 +113,7 @@ export async function POST(req: NextRequest) {
 
 }
 
-export async function OPTIONS(request: NextRequest) {
+export async function OPTIONS() {
   return new NextResponse(null, {
     status: 204,
     headers: {
